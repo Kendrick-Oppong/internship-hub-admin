78a057a04461dbbcd12efc0b4d372c0594dad1dd
Kendrick <kendrickoppong94@gmail.com>
2026-02-23 12:18:30 +0000

    setup student query hook

---
+++ lib/hooks/queries/use-student-queries.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { studentApi } from "@/lib/api/queries/student";
+import { Query_Keys } from "@/lib/constants/query-keys";
+import { ApiQueryParams } from "@/types/common/filter-config";
+
+export const useGetAllStudents = (params: ApiQueryParams) => {
+  return useQuery({
+    queryKey: Query_Keys.students.all(params),
+    queryFn: () => studentApi.getAllStudents(params),
+  });
+};
