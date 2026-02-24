949a64323d7f2f2203463a5390c205e8535176f0
Kendrick <kendrickoppong94@gmail.com>
2026-02-24 22:24:00 +0000

    add getStudentById to config

--- lib/api/queries/student.ts
+++ lib/api/queries/student.ts
@@ -1,6 +1,6 @@
 import { api } from "@/lib/api/axios";
 import { API_ENDPOINTS } from "@/lib/constants/api-endpoints";
-import { StudentsResponse } from "@/types/api/student";
+import { StudentsResponse, Student } from "@/types/api/student";
 import { ApiQueryParams } from "@/types/common/filter-config";
 
 export const studentApi = {
@@ -8,6 +8,12 @@ export const studentApi = {
     const response = await api.get<StudentsResponse>(
       API_ENDPOINTS.PROFILE.STUDENTS,
       { params }
+    );
+    return response.data;
+  },
+  getStudentById: async (id: string) => {
+    const response = await api.get<Student>(
+      API_ENDPOINTS.PROFILE.STUDENT_DETAIL(id)
     );
     return response.data;
   },
