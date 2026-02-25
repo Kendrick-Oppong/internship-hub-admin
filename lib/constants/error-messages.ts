e040670edefe34d3baa4ab792b08221665dec5ed
Kendrick <kendrickoppong94@gmail.com>
2026-02-25 21:40:50 +0000

    add supervisors error message

--- lib/constants/error-messages.ts
+++ lib/constants/error-messages.ts
@@ -1,5 +1,6 @@
 export const ERROR_MESSAGES = {
   INTERNSHIP_PERIODS: "Failed to load internship periods",
   STUDENTS: "Failed to load students",
+  SUPERVISORS: "Failed to load supervisors",
   GENERIC: "Failed to load data",
 };
